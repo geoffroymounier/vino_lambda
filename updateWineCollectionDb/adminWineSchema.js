243c1ff7de1f333b38d10779002aa9fb3e90c8d0
Geoffroy Mounier <geoffroymounier@gmail.com>
2020-04-27 18:57:19 +0100

    updateEnv Variables

--- updateWineCollectionDb/adminWineSchema.js
+++ updateWineCollectionDb/adminWineSchema.js
@@ -2,27 +2,29 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const ObjectId = Schema.Types.ObjectId
 
-export default (new Schema
-  {
-      photo:String,
-      appelation: String,
-      domain: String,
-      temperature: String,
-      photo : { type: Buffer,contentType: String },
-      annee : {type:Number,min:1900,max:2050},
-      before : {type:Number,min:1900,max:2050},
-      apogee : {type:Number,min:1900,max:2050},
-      carafage : String,
-      typologie : String,
-      country:String,
-      region:String,
-      color:String,
-      vendor:String,
-      bouche:[String],
-      pastilles:[String],
-      accords:[String],
-      cepage:[String],
-      vue:[String]
-  },
-  {timestamps:true}
-)
+module.exports = {
+  AdminWineSchema : new Schema(
+    {
+        photo:String,
+        appelation: String,
+        domain: String,
+        temperature: String,
+        photo : { type: Buffer,contentType: String },
+        annee : {type:Number,min:1900,max:2050},
+        before : {type:Number,min:1900,max:2050},
+        apogee : {type:Number,min:1900,max:2050},
+        carafage : String,
+        typologie : String,
+        country:String,
+        region:String,
+        color:String,
+        vendor:String,
+        bouche:[String],
+        pastilles:[String],
+        accords:[String],
+        cepage:[String],
+        vue:[String]
+    },
+    {timestamps:true}
+  )
+}
