b56501f0441798e255171afef645c56f08ec220b
Geoffroy Mounier <geoffroymounier@gmail.com>
2020-05-03 19:57:52 +0100

    uploadWine DB

--- updateWineCollectionDb/index.js
+++ updateWineCollectionDb/index.js
@@ -1,19 +1,28 @@
 const AWS = require('aws-sdk');
 const csv = require('csvtojson');
-const mongoose = require('mongoose')
-
-const { generateResponse, createConnection } = require('/opt/nodejs/util')
-const {DBUSER,DBPASS,DBCLUSTER} =  process.env
+const mongoose = require('mongoose');
+const { generateResponse, createConnection } = require('/opt/nodejs/util');
+const {DBUSER,DBPASS,DBCLUSTER} =  process.env;
 const s3 = new AWS.S3();
 
-const {AdminWineSchema} = require('./adminWineSchema.js')
+const {AdminWineSchema} = require('./adminWineSchema.js');
 
 let conn = null;
-let AdminWine = null;
+let AdminWineModel = null;
 
 exports.handler = async (event, context,callback) => {
-  context.callbackWaitsForEmptyEventLoop = false
+  context.callbackWaitsForEmptyEventLoop = false;
 
+  // connect to DB
+  if (conn == null) {
+    conn = await createConnection({DBUSER,DBPASS,DBCLUSTER});
+    conn.model('Admin-Wine', new mongoose.Schema(AdminWineSchema));
+    AdminWineModel = conn.model('Admin-Wine');
+  } else {
+    console.log('cached');
+  }
+
+  // connect to Bucket
   const srcBucket = event.Records[0].s3.bucket.name;
   const srcKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
   const params = {
@@ -21,42 +30,19 @@ exports.handler = async (event, context,callback) => {
     Key: srcKey
   };
 
-  if (conn == null) {
-    conn = await createConnection({DBUSER,DBPASS,DBCLUSTER});
-    conn.model('Admin-Wine', new mongoose.Schema(AdminWineSchema));
-    AdminWine = conn.model('Admin-Wine');
-  } else {
-    console.log('cached')
+  try {
+    const stream = await s3.getObject(params).createReadStream();
+    const entries = (await csv().fromStream(stream)).reduce((arr,entry) =>
+      [...arr,{
+        _id : entry._id || new mongoose.Types.ObjectId(),
+        ...entry
+      }],[]);
+      const adminWine = new AdminWineModel();
+      const wines = await adminWine.save(entries);
+      console.log({wines});
+      generateResponse(callback,{message:entries.length + ' wine(s) successfully uploaded.'});
+  } catch (err){
+    console.log({err});
+    generateResponse(callback,{Error: err,Reference: context.awsRequestId},500);
   }
-
-
-  const stream = await s3.getObject(params).createReadStream();
-
-  var adminWines = []
-
-  csv().fromStream(stream).on('data', (data) => {
-   const json = JSON.parse(new Buffer(data).toString(“utf8”));
-   console.log(json)
-  })
-  // try {
-  //
-  //   csv.fromString(csvFile.data.toString(), {
-  //        headers: true,
-  //        ignoreEmpty: true
-  //    })
-  //    .on("data", function(data){
-  //        data['_id'] = data['_id'] || new mongoose.Types.ObjectId();
-  //        adminWines.push(data);
-  //    })
-  //    .on("end", function(){
-  //        AdminWine.save(adminWines, function(err, documents) {
-  //           if (err) throw err;
-  //        });
-  //        generateResponse(callback,{message:adminWines.length + ' wine have been successfully uploaded.'})
-  //    });
-  //
-  // } catch(err) {
-  //   generateResponse(callback,{Error: err,Reference: context.awsRequestId},500)
-  // }
-
-}
+};
